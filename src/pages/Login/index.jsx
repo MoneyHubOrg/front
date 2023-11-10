4caa1794f4bf04b86c3d688ad2c075f99884fe85
Guilhermedcdias <Guilhermedcdias.2021@gmail.com>
2023-11-10 08:10:29 -0300

    sla oq eu fiz

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,6 +1,9 @@
 import { useNavigation } from '@react-navigation/native';
 import React from 'react';
-import { View, Text, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, TextInput, TouchableOpacity, Dimensions } from 'react-native';
+import Svg, { Image, Path } from 'react-native-svg';
+import MoneyHub1 from '../../../assets/MoneyHub1.png';
+
 
 export default function Login() {
 
@@ -13,24 +16,38 @@ export default function Login() {
     const logar = () => {
         navegar.navigate('Main');
     }
+    // Obter a largura da tela
+    const screenWidth = Dimensions.get('window').width;
 
+    // Calcular a altura correspondente mantendo a proporção do SVG original
+    const svgHeight = (screenWidth * 902) / 1081;
     return (
         <View style={LoginStyle.container}>
-            <Text style={LoginStyle.titulo}>Login </Text>
+            <View style={LoginStyle.imagemFundo}>
+                <Svg xmlns="http://www.w3.org/2000/svg" width={screenWidth} height={svgHeight} viewBox="0 0 1081 902" fill="none">
+                    <Path d="M0.414724 901.713L114.958 835.604L165.415 801.713L194.915 784.214L229.502 769.495L400.63 716.407L486.194 689.864L529.915 673.713L558.454 663.96C565.737 661.471 573.283 659.831 580.942 659.072L619.915 655.213L670.336 648.941L768.915 634.563L867.493 620.184L916.782 612.995L947.164 607.925C955.958 606.457 964.517 603.82 972.612 600.083L994.657 589.908L1023.24 574.01L1080.41 542.214L1080.41 0.713299L0.414722 0.713476L0.414724 901.713Z" fill="#000" />
+                </Svg>
+            </View>
+            <Text style={LoginStyle.titulo}>Bem vindo de volta </Text>
+            {/* inserindo imagem */}
+            <Image source={MoneyHub1} style={{ width: 100, height: 100 }} />
             <View style={LoginStyle.inputsContainer}>
-                <TextInput style={LoginStyle.input} placeholder="Email" />
+                {/* label */}
+                <Text style={LoginStyle.label}>Usuário</Text>
+                <TextInput style={LoginStyle.input} placeholder="Usuario" />
+                <Text style={LoginStyle.label}>Senha</Text>
                 <TextInput style={LoginStyle.input} placeholder="Senha" />
                 <TouchableOpacity style={LoginStyle.button} onPress={() => {
                     logar();
-                 }}>
+                }}>
                     <Text style={LoginStyle.buttonText}>Entrar </Text>
                 </TouchableOpacity>
             </View>
             <View style={LoginStyle.SemCadastro}>
                 <Text style={LoginStyle.textoSemCadastro}>Não tem cadastro? </Text>
-                <TouchableOpacity onPress={() => { 
+                <TouchableOpacity onPress={() => {
                     cadastrar();
-                 }}>
+                }}>
                     <Text style={LoginStyle.textoSemCadastro}>Cadastre-se </Text>
                 </TouchableOpacity>
             </View>
@@ -43,46 +60,77 @@ export default function Login() {
 const LoginStyle = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#000',
+        backgroundColor: '#e6e6e6',
         alignItems: 'center',
-        justifyContent: 'center',
+        justifyContent: 'space-between',
+    },
+
+    imagemFundo: {
+        height: '100vh',
+        flex: 1,
+        justifyContent: 'flex-start',
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        zIndex: -1,
     },
 
     inputsContainer: {
         width: '90%',
-        backgroundColor: '#333333',
         padding: 20,
         paddingTop: 40,
         paddingBottom: 40,
         borderRadius: 5,
+        marginTop: 130,
+        justifyContent: 'center',
+        alignItems: 'center',
     },
 
     titulo: {
-        fontSize: 30,
-        color: '#fff',
-        marginBottom: 20,
+        fontSize: 25,
+        fontWeight: '700',
+        fontFamily: 'Passion One Black',
+        color: '#ffffff',
+        marginTop: 30,
     },
 
     input: {
-        backgroundColor: '#ffffff19',
+        backgroundColor: '#0000002d',
         borderRadius: 5,
         marginBottom: 10,
         padding: 10,
+        width: '100%',
+    },
+    label: {
+        width: '100%',
+        color: '#000000',
+        marginBottom: 10,
+        paddingLeft: 5,
+        fontSize: 15,
+        fontWeight: '700',
     },
     button: {
-        backgroundColor: '#00611a',
+        marginTop: 20,
+        backgroundColor: '#000000',
         padding: 10,
+        width: '50%',
         borderRadius: 5,
         alignItems: 'center',
         justifyContent: 'center',
+        // marginBottom: 25,
     },
     buttonText: {
         color: '#fff',
         fontSize: 18,
+        // marginBottom: 25,
     },
     SemCadastro: {
         flexDirection: 'row',
         marginTop: 10,
+        backgroundColor: '#000000',
+        padding: 20,
+        width: '100%',
+        justifyContent: 'center',
     },
     textoSemCadastro: {
         color: '#fff',
