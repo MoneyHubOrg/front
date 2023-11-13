e5498bb54e119f4c9d95b9e5b92b3f9fb25fb742
KevinRomRib <romribkevin@gmail.com>
2023-11-13 09:14:42 -0300

    firebase

--- App.jsx
+++ App.jsx
@@ -2,14 +2,6 @@ import React from 'react';
 
 import { NavigationContainer } from '@react-navigation/native'
 import AppNavigator from './src/routes';
-import { AppRegistry } from 'react-native';
-import App from './App';
-import { name as appName } from './app.json';
-import { getFirebase } from '@react-native-firebase/app';
-
-
-AppRegistry.registerComponent(appName, () => App);
-getFirebase();
 
 export default function App(){
   return(
