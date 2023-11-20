d636356dba4486d345ff335eea7a1c9c1e38178b
LucasMedici <lucasrpmedici@gmail.com>
2023-11-20 08:26:11 -0300

    adicionando saldo no menu perfil

--- src/pages/Perfil/index.js
+++ src/pages/Perfil/index.js
@@ -2,9 +2,49 @@ import React, {useContext} from 'react';
 import { View, StyleSheet, Image, Text } from 'react-native';
 import Profile from "../../../assets/img/profile.png"
 import { AuthContext } from '../../contexts/AuthContext';
+import { useState, useEffect } from 'react';
+import firestore from '@react-native-firebase/firestore';
+
+import AntDesign from 'react-native-vector-icons/AntDesign';
+
+          
+
 
 export default function Perfil() {
   const { user } = useContext(AuthContext);
+  const [saldo, setSaldo] = useState()
+
+  
+  const formatCurrency = (value) => {
+    let currency = (parseFloat(value)).toLocaleString('pt-BR', {
+        style: 'currency',
+        currency: 'BRL'
+    });
+    return currency
+}
+
+  async function FuncaoProcura() {
+    const collectionRef = firestore().collection('conta').where('email', '==', user.email);
+  
+      collectionRef.get()
+        .then(snapshot => {
+          snapshot.forEach(doc => {
+            setSaldo(doc.data().saldo_em_conta)
+          });
+        
+    
+        })
+        .catch(err => {
+          console.error('Erro ao obter documentos da coleção:', err);
+  
+        });
+        
+        }
+  
+
+  useEffect(() => {
+    FuncaoProcura()
+  }, []) 
 
   return (
       <View style={stylesPerfil.container}>
@@ -15,13 +55,21 @@ export default function Perfil() {
           <Text style={stylesPerfil.Text}>{user.displayName}</Text>
         </View>
         <View style={stylesPerfil.info}>
-          <Text style={stylesPerfil.infoTitle}>Informações da Conta</Text>
+          <Text style={stylesPerfil.infoTitle}>Informações do Usuário</Text>
           <View style={stylesPerfil.informations}>
             <Text style={stylesPerfil.infoText}>Nome: {user.displayName}</Text>
             <Text style={stylesPerfil.infoText}>E-mail: {user.email}</Text>
-            {/* <Text style={stylesPerfil.infoText}>Telefone: (12) 99154-7409</Text> */}
           </View>
         </View>
+
+        <View style={stylesPerfil.info}>
+          <Text style={stylesPerfil.infoTitle}>Informações da Conta</Text>
+          <View style={stylesPerfil.informationsCount}>
+            <Text style={stylesPerfil.infoText}>Saldo: {formatCurrency(saldo)}</Text>
+            <AntDesign name="edit" color='white' size={20} />
+          </View>
+        </View>
+        
       </View>
   )
 }
@@ -33,6 +81,7 @@ const stylesPerfil = StyleSheet.create({
     backgroundColor: 'black',
     height: '100%',
     paddingBottom: 10,
+    gap: 10
   },
   infoProfile: {
     alignItems: 'center',
@@ -54,10 +103,8 @@ const stylesPerfil = StyleSheet.create({
     fontSize: 25,
   },
   info: {
-    flex: 1,
-    flexDirection: 'column',
     width: '90%',
-    height: '100%',
+    height: '20%',
     backgroundColor: '#333333',
     borderRadius: 30,
     paddingLeft: 50,
@@ -69,6 +116,12 @@ const stylesPerfil = StyleSheet.create({
     fontSize: 20,
   },
   informations: {
+    paddingTop: 20,
+    gap: 10,
+  },
+  informationsCount: {
+    flex:1,
+    flexDirection: 'row',
     paddingTop: 20,
     gap: 10,
   },
