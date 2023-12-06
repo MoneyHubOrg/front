f4c5c28474e05fc1edfdf686de54785fb09c8fc6
LucasMedici <lucasrpmedici@gmail.com>
2023-12-06 01:00:17 -0300

    comecando os relatorios, falta botar dados reais

--- src/pages/Relatorios/index.js
+++ src/pages/Relatorios/index.js
@@ -1,50 +1,124 @@
-import React, {useEffect, useState} from 'react';
-import { View, Text, StyleSheet, TextInput, Button } from 'react-native';
-import {
-  BarChart,
-} from 'react-native-chart-kit';
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { BarChart } from 'react-native-chart-kit';
 import { Dimensions } from 'react-native';
 
-
-
-export default function Relatorios(){
-
+export default function Relatorios() {
   const screenWidth = Dimensions.get('window').width;
 
-  const data = {
-    labels: ['January', 'February', 'March', 'April', 'May', 'June'],
-    datasets: [{
-      data: [20, 45, 28, 80, 99, 43]
-    }]
+  const dataReceita = {
+    labels: ['Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'],
+    datasets: [
+      {
+        data: [22, 50, 75, 60, 30, 85],
+      },
+    ],
   };
 
-  const chartConfig = {
-    backgroundGradientFrom: '#1E2923',
-    backgroundGradientTo: '#08130D',
-    color: (opacity = 1) => `rgba(26, 255, 146, ${opacity})`,
-    strokeWidth: 2
+  const dataDespesa = {
+    labels: ['Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'],
+    datasets: [
+      {
+        data: [22, 50, 75, 60, 30, 85],
+      },
+    ],
   };
 
-  const MyLineChart = () => (
+  const GraficoReceita = () => (
     <BarChart
-      data={data}
-      width={screenWidth}
+      data={dataReceita}
+      width={screenWidth - 20} 
       height={220}
-      chartConfig={chartConfig}
+      yAxisLabel="R$"
+      chartConfig={{
+        backgroundColor: '#1f7a1f',
+        backgroundGradientFrom: '#138d13',
+        backgroundGradientTo: '#33cc33',
+        decimalPlaces: 2,
+        color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+        labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+        style: {
+          borderRadius: 16,
+        },
+        propsForDots: {
+          r: '6',
+          strokeWidth: '2',
+          stroke: '#33cc33',
+        },
+      }}
+      style={{
+        marginVertical: 8,
+        borderRadius: 16,
+      }}
     />
   );
 
-  return(
+  const GraficoDespesa = () => (
+    <BarChart
+      data={dataReceita}
+      width={screenWidth - 20} 
+      height={220}
+      yAxisLabel="R$"
+      chartConfig={{
+        backgroundColor: '#990000',
+        backgroundGradientFrom: '#cc0000',
+        backgroundGradientTo: '#ff3333',
+        decimalPlaces: 2,
+        color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+        labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+        style: {
+          borderRadius: 16,
+        },
+        propsForDots: {
+          r: '6',
+          strokeWidth: '2',
+          stroke: '#33cc33',
+        },
+      }}
+      style={{
+        marginVertical: 8,
+        borderRadius: 16,
+      }}
+    />
+  );
+
+
+  return (
     <View style={styles.container}>
-      <MyLineChart />
+      <View style={styles.title}>
+        <Text style={styles.text}>Relatórios</Text>
+        <View
+          style={{
+            borderBottomColor: '#535353',
+            borderBottomWidth: StyleSheet.hairlineWidth,
+            width: '100%',
+            paddingBottom: 10,
+          }}
+        />
+      </View>
+      <GraficoReceita />
+      <GraficoDespesa />
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
-  container:{
-    height: '100%',
-    alignItems: 'flex-start',
-    backgroundColor: 'black',
+  container: {
+    flex: 1,
+    backgroundColor: '#333333',
+    padding: 10,
   },
-})
+  title: {
+    paddingTop: 30,
+    width: '100%',
+    alignItems: 'flex-start',
+    paddingRight: 10,
+  },
+  text: {
+    width: '100%',
+    paddingLeft: 20,
+    color: 'white',
+    fontSize: 30,
+    fontWeight: 'bold',
+  },
+});
